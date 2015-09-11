e2e99106232e1fa9903ad3bc754812947520c138
tesselode <aminnich3@gmail.com>
2015-09-11 12:19:17 -0400

    Circles appear to be working

--- main.lua
+++ main.lua
@@ -1,12 +1,12 @@
 function love.load()
   drawboxes = require 'drawboxes'
 
-  rect1 = drawboxes.Rectangle(10, 10, 500, 500, 'fill', {255, 0, 0, 255})
+  circle = drawboxes.Circle(100, 200, 100, 100, 'fill', {255, 0, 0, 255})
   rect2 = drawboxes.Rectangle(10, 10, 30, 30, 'line')
-  rect2:setCenter(rect1:getCenter())
+  rect2:setCenter(circle:getRight(), circle:getCenterY())
 end
 
 function love.draw()
-  rect1:draw()
+  circle:draw()
   rect2:draw()
 end
