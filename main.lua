6b38261abbed0e11ae96f446362392f635cb96bc
tesselode <aminnich3@gmail.com>
2015-09-27 16:02:38 -0400

    Added text example to the readme

--- main.lua
+++ main.lua
@@ -1,19 +1,28 @@
 function love.load()
   drawboxes = require 'drawboxes'
 
-  font1 = love.graphics.newFont('kenpixel.ttf', 30)
-  font2 = love.graphics.newFont('kenvector_future.ttf', 60)
+  local lg = love.graphics
 
-  mainContainer = drawboxes.Container(0, 0, love.graphics.getWidth(), love.graphics.getHeight())
+  --load some fonts
+  font1 = lg.newFont('kenpixel.ttf', 30)
+  font2 = lg.newFont('kenvector_future.ttf', 60)
 
+  --this container holds everything else and takes care of the screen size
+  mainContainer = drawboxes.Container(0, 0, lg.getWidth(), lg.getHeight())
+
+  --make some text objects
   text1 = drawboxes.Text(font1, 'Hello', 0, 0)
   text2 = drawboxes.Text(font2, 'World!', 0, 0)
+
+  --position text2 relative to text1
   text2:setLeft(text1:getRight())
   text2:setCenterY(text1:getCenterY())
 
-  textContainer = drawboxes.Container(0, 0, love.graphics.getWidth(), love.graphics.getHeight())
+  --center the text on the screen
+  textContainer = drawboxes.Container(0, 0, lg.getWidth(), lg.getHeight())
   textContainer:wrap(text1, text2)
-  textContainer:setCenter(400, 300)
+  textContainer:setCenter(mainContainer:getCenter())
+
   mainContainer:add(textContainer)
 end
 
--- readme.md
+++ readme.md
@@ -2,6 +2,43 @@ Drawboxes
 =========
 Drawboxes is a library that represents drawing operations as boxes and gives you different ways to position those boxes relative to each other.
 
+Examples
+--------
+### Text example
+```lua
+function love.load()
+  drawboxes = require 'drawboxes'
+
+  local lg = love.graphics
+
+  --load some fonts
+  font1 = lg.newFont('kenpixel.ttf', 30)
+  font2 = lg.newFont('kenvector_future.ttf', 60)
+
+  --this container holds everything else and takes care of the screen size
+  mainContainer = drawboxes.Container(0, 0, lg.getWidth(), lg.getHeight())
+
+  --make some text objects
+  text1 = drawboxes.Text(font1, 'Hello', 0, 0)
+  text2 = drawboxes.Text(font2, 'World!', 0, 0)
+
+  --position text2 relative to text1
+  text2:setLeft(text1:getRight())
+  text2:setCenterY(text1:getCenterY())
+
+  --center the text on the screen
+  textContainer = drawboxes.Container(0, 0, lg.getWidth(), lg.getHeight())
+  textContainer:wrap(text1, text2)
+  textContainer:setCenter(mainContainer:getCenter())
+
+  mainContainer:add(textContainer)
+end
+
+function love.draw()
+  mainContainer:draw()
+end
+```
+
 API
 ---
 ### Base class
